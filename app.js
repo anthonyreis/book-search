3cef4706c2ac2a60d57ec0d378aa31cd2dd62125
anthonyreis <matheus.anthony1@gmail.com>
2021-05-24 15:33:52 -0300

    mais ajustes do front

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ const partialsPath = path.join(__dirname, './templates/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
+hbs.registerHelper('isdivisible', function (value) {
+
+    return Math.floor(value/2) == 0;
+  });
+  
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
